da6ab79120c88513738b58fc6e8bcb64176de7ad
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2024-05-28 18:43:19 -0300

    Criar e editar postagem

--- src/pages/CriarPubli/CriarPubli.jsx
+++ src/pages/CriarPubli/CriarPubli.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from './CriarPubli.module.css'
 
-export const CriarPubli = () => {
-  return (
-    <div>CriarPubli</div>
-  )
+function CriarPubli() {
+    
+    
+
+
+    // https://www.flaticon.com/br/icone-gratis/voltar_3585896?term=voltar&page=1&position=1&origin=tag&related_id=3585896
+    return (
+        <div className={style["container-publicacao"]}>
+            <div className={style["box-publicacao"]}>
+                <div className={style["titulo-publicacao"]}>
+                    <a href=""><img src="../../../public/img/voltar.png" alt="" /></a>
+                    <h1>Criar postagem</h1>
+                </div>
+                <form action="" className={style["form-publicacao"]}>
+                    <label htmlFor="" >Título:
+                        <input className={style['publicacao-input-titulo']} type="text" placeholder="título da sua postagem"/>
+                    </label>
+                    <label htmlFor="">Descrição:
+                        <input className={style['publicacao-input-descricacao']} type="text" placeholder='conte-nos sobre sua postagem' />
+                        
+                    </label>
+                    <button>Escolha uma imagem:</button>
+                    <p>Nenhum upload selecionado*</p>
+
+                </form>
+                <button className={style["btn-criacao-publi"]}>Criar publicação</button>
+            </div>
+        </div>
+    )
 }
-export default CriarPubli;
+
+export default CriarPubli
